10ab7ad199ea1f5a56e6338be0813b238a7d3706
xtyuns <xtyuns@163.com>
2021-08-23 19:04:51 +0800

    add Index  MusicList

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const baseURL = 'http://localdev:3000';
+
 /**
  * 获取首页 Banner 列表
  * @param type 设备类型, 1: PC、2: Android、3: iPhone、4: iPad
@@ -8,10 +10,28 @@ import axios from 'axios'
 export async function getBanners(type = 2) {
     const banners: string[] = [];
     try {
-        const res = await axios.get(`http://localdev:3000/banner?type=${type}`);
+        const res = await axios.get(`${baseURL}/banner?type=${type}`);
         banners.push(...(<any[]> res.data.banners).map(item => item.pic));
     } catch (e) {
-        console.error('获取 banner 列表失败: ' + e.message);
+        console.error('获取首页 banner 列表失败: ' + e.message);
     }
     return banners;
+}
+
+
+/**
+ * 获取首页歌单列表
+ * @param limit 列表数量
+ * @returns 歌单对象列表
+ */
+export async function getMusicList(limit = 10) {
+    const playLists = [];
+    try {
+        const res = await axios.get(`${baseURL}/top/playlist?limit=${limit}`);
+        playLists.push(...((<any[]> res.data.playlists).map(item => ({name: item.name, coverImgUrl: item.coverImgUrl}))));
+    }   catch (e) {
+        console.error('获取首页歌单列表失败: ' + e.message);
+    }
+
+    return playLists;
 }
--- src/components/MusicList.vue
+++ src/components/MusicList.vue
@@ -4,15 +4,39 @@
         <div class="title">发现好歌单</div>
         <div class="more">查看更多</div>
     </div>
+
+    <swiper
+        class="music-list-items"
+        slides-per-view="auto"
+        :free-mode="true"
+        :space-between="10"
+    >
+        <swiper-slide class="music-list-item" v-for="(item, idx) in musicList" :key="idx">
+            <img :src="item.coverImgUrl" alt="">
+            <span>{{ item.name }}</span>
+        </swiper-slide>
+    </swiper>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeMount, ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/swiper.less';
+import { getMusicList } from '../api/index';
+
 export default defineComponent({
     name: 'MusicList',
+    components: {
+        Swiper,
+        SwiperSlide
+    },
     setup: () => {
-        return {}
+        const musicList = ref(<any[]> []);
+        onBeforeMount(async() => {
+            musicList.value = await getMusicList();
+        });
+        return { musicList }
     }
 })
 </script>
@@ -22,6 +46,7 @@ export default defineComponent({
     padding: .2rem;
 
     .music-list-top {
+        padding: 0 0 .15rem;
         display: flex;
         justify-content: space-between;
 
@@ -31,10 +56,35 @@ export default defineComponent({
         }
 
         .more {
-            border: 1px solid #ccc;
+            border: 1px solid #999;
+            color: #999;
             padding: .02rem .16rem;
             border-radius: .2rem;
             font-size: .24rem;
+        }
+    }
+
+    .music-list-items {
+        .music-list-item {
+            display: flex;
+            flex-direction: column;
+            width: 2rem;
+            img {
+                width: 100%;
+                height: 2rem;
+                border-radius: .1rem;
+            }
+            span {
+                margin: .1rem 0 0;
+                font-size: .24rem;
+                line-height: .32rem;
+                height: .64rem;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                text-overflow: ellipsis;
+                overflow: hidden;
+            }
         }
     }
 }
