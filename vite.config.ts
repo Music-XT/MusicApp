4115de89b216271a7f3a7eda93d95cacc31e46e7
xtyuns <xtyuns@163.com>
2021-08-27 20:18:28 +0800

    [note] config env setting
    I think this is a very useful operation.
    It helps us to configure variables in different environments.

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,30 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+const baseConfig= defineConfig({
+  plugins: [vue()]
+});
+
+// development
+const devConfig = defineConfig({
   server: {
     host: '0.0.0.0',
     port: 80
-  },
+  }
+});
+
+// production
+const proConfig = defineConfig({
   build: {
     outDir: 'gh-pages'
   },
   base: '/MusicApp/'
+});
+
+const DEV = 'development'
+const PRO = 'production'
+export default defineConfig(({ command, mode }) => {
+  if(mode === DEV) return {...baseConfig, ...devConfig}
+  if(mode === PRO) return {...baseConfig, ...proConfig}
+  return { ...baseConfig}
 })
