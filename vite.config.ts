e001badc191964c0cf7a8855d202c0536858bb09
xtyuns <xtyuns@163.com>
2021-08-28 14:22:17 +0800

    add gh-pages build script in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-const baseConfig= defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  }
-});
 
-// development
-const devConfig = defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 80
-  }
-});
-
-// production
-const proConfig = defineConfig({
-  build: {
-    outDir: 'gh-pages'
-  },
-  base: '/MusicApp/'
-});
-
+// 默认环境名称
 const DEV = 'development'
 const PRO = 'production'
-export default defineConfig(({ command, mode }) => {
-  if(mode === DEV) return {...baseConfig, ...devConfig}
-  if(mode === PRO) return {...baseConfig, ...proConfig}
-  return { ...baseConfig}
-})
+
+const config: {base: UserConfigExport, [prop: string]: UserConfigExport}  = {
+  // https://vitejs.dev/config/
+  base: {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    }
+  },
+  [DEV]: {
+    server: {
+      host: '0.0.0.0',
+      port: 80
+    }
+  },
+  [PRO]: {
+  }
+}
+
+// 根据项目环境配置 vite.config.ts
+export default defineConfig(({ mode }) => ({...(config.base), ...(config[mode])}))
