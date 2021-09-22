eb1e1ce653baae59facbdbd0a7bb7376e7f4e5a1
xtyuns <xtyuns@163.com>
2021-09-22 10:03:48 +0800

    prepare for playList and playDetail

--- src/views/home/components/NewSongs.vue
+++ src/views/home/components/NewSongs.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="new-songs">
-    <div class="title">推荐新歌<span> ({{newSongs.length??0}})</span></div>
+    <div class="title">推荐新歌<span> ({{newSongs?.length??0}})</span></div>
     <div class="songs">
         <div class="song" v-for="(item, idx) in newSongs" :key="idx" @click="insertPlay(item)">
             <div class="pic">
--- src/views/home/components/TopNav.vue
+++ src/views/home/components/TopNav.vue
@@ -1,14 +1,6 @@
 <template>
 <div class="top-nav">
-    <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-nav"></use>
-    </svg>
-    <div class="top-nav-center">
-        <span>我的</span>
-        <span class="active">发现</span>
-        <span>云村</span>
-        <span>视频</span>
-    </div>
+    <div class="top-nav-left">Online Music</div>
     <svg class="icon" aria-hidden="true">
         <use xlink:href="#icon-search"></use>
     </svg>
@@ -19,19 +11,19 @@
 .top-nav {
     width: 100%;
     height: .85rem;
+    position: sticky;
+    left: 0;
+    top: 0;
+    background-color: #fff;
+    z-index: 999;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0 .2rem;
     
-    .top-nav-center {
-        flex-grow: .5;
-        display: flex;
-        justify-content: space-evenly;
-
-        .active {
-            font-weight: 700;
-        }
+    .top-nav-left {
+        font-size: .28rem;
+        font-weight: bolder;
     }
 }
 </style>
